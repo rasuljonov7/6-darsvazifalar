bea3e411a712a0138a04447d99bf05b8628a01ea
rasuljonov7 <rasuljonov7>
2025-04-09 11:48:03 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,150 @@
+// 1-masala
+
+// let input = +prompt("1-7 gacha son kirgizing");
+// if (input == 1) {
+//   console.log("Dushanba");
+// } else if (input == 2) {
+//   console.log("seshanba");
+// } else if (input == 3) {
+//   console.log("Chorshanba");
+// } else if (input == 2) {
+//   console.log("Payshanba");
+// } else if (input == 2) {
+//   console.log("Juma");
+// } else if (input == 2) {
+//   console.log("shanba");
+// } else {
+//   console.log("Yakshanba");
+// }
+
+// 2 - masala
+// let score = 30;
+// if (score >= 90) {
+//   console.log("Yaxshi");
+// } else if (score > 50) {
+//   console.log("O'rtacha");
+// } else console.log("Yomon");
+
+// 3-masala
+// let age = 30;
+// if (age < 18) {
+//   console.log("");
+// } else if (age <= 30) {
+//   console.log("salom yoshlar");
+// } else {
+//   console.log("Salom katta odam");
+// }
+
+// 4-masala
+// let day = 7;
+// if (day == 1) console.log("Dushanba");
+// else if (day == 2) console.log("Seshanba");
+// else if (day == 3) console.log("Chorshanba");
+// else if (day == 4) console.log("Payshanba");
+// else if (day == 5) console.log("Juma");
+// else if (day == 6) console.log("shanba");
+// else console.log("Yakshanba");
+
+// 5-masala
+// let grade = 92;
+// if (grade > 100) {
+//   console.log("Bunday ball mavjud emas maximalni 100 ball");
+// } else if (grade >= 90) {
+//   console.log("A");
+// } else if (grade >= 75) {
+//   console.log("B");
+// } else if (grade >= 50) {
+//   console.log("C");
+// } else console.log("D");
+
+// 6-masala
+// let num = -3;
+// if (num < 0) console.log("manfiy son");
+// else if (num > 0) console.log("musbat son");
+// else console.log("nol");
+
+// 7-masala
+// let day = 6;
+// if (day == 1) console.log("ish kuni");
+// else if (day == 2) console.log("ish kuni");
+// else if (day == 3) console.log("ish kuni");
+// else if (day == 4) console.log("ish kuni");
+// else if (day == 5) console.log("ish kuni");
+// else if (day == 6) console.log("Dam olish kuni");
+// else console.log("Dam olish kuni");
+
+// 8-masala
+// let grade = 69;
+// if (grade > 100) {
+//   console.log("Bunday ball mavjud emas maximalni 100 ball");
+// } else if (grade >= 90) {
+//   console.log("A'lo");
+// } else if (grade >= 75) {
+//   console.log("Yaxshi");
+// } else if (grade >= 50) {
+//   console.log("Qoniqarli");
+// } else console.log("Qoniqarsiz");
+
+// 9-masala
+// let password = 1234;
+// if (password == 1234) console.log("Parol to'gri");
+// else console.log("Parol notogri");
+
+// 10-masala
+// let age = 17;
+// if (age < 18) console.log("Ro'yxatdan o'ta olmaysiz");
+// else console.log("Ro'yxatdan o'ta olasiz");
+
+//  QOSHIMCHA
+// const input = prompt("1 dan 999 gacha bo‘lgan son kiriting:");
+
+// const birlik = [
+//   "",
+//   "bir",
+//   "ikki",
+//   "uch",
+//   "to'rt",
+//   "besh",
+//   "olti",
+//   "yetti",
+//   "sakkiz",
+//   "to'qqiz",
+// ];
+// const onlar = [
+//   "",
+//   "o'n",
+//   "yigirma",
+//   "o'ttiz",
+//   "qirq",
+//   "ellik",
+//   "oltmish",
+//   "yetmish",
+//   "sakson",
+//   "to'qson",
+// ];
+
+// const son = input;
+
+// if (son >= 1 && son <= 999) {
+//   let natija = "";
+
+//   const yuzlik = Math.floor(son / 100);
+//   const onlik = Math.floor((son % 100) / 10);
+//   const birliklik = son % 10;
+
+//   if (yuzlik > 0) {
+//     natija += birlik[yuzlik] + " yuz ";
+//   }
+
+//   if (onlik > 0) {
+//     natija += onlar[onlik] + " ";
+//   }
+
+//   if (birliklik > 0) {
+//     natija += birlik[birliklik];
+//   }
+
+//   alert(natija.trim());
+// } else {
+//   alert("Iltimos, 1 dan 999 gacha bo'lgan son kiriting.");
+// }
